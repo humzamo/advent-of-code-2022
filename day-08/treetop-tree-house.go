c04bb629ef24c16ae2e5b3b1fd50ddcd42dd5736
Humza Moazzam <humza.moazzam@atos.net>
2022-12-13 20:40:47 +0000

    code tidy up

--- day-08/treetop-tree-house.go
+++ day-08/treetop-tree-house.go
@@ -11,12 +11,8 @@ import (
 func main() {
 	grid := loadInput("Input.txt")
 
-	// print out grid
-	// for _, l := range grid {
-	// 	fmt.Println(l)
-	// }
-	// fmt.Println("The answer to part one is:", partOne(grid))
-	fmt.Println("The answer to part one is:", partTwo(grid))
+	fmt.Println("The answer to part one is:", partOne(grid))
+	fmt.Println("The answer to part two is:", partTwo(grid))
 }
 
 func loadInput(inputFileName string) [][]int {
@@ -39,30 +35,28 @@ func loadInput(inputFileName string) [][]int {
 	return output
 }
 
-// func partOne(grid [][]int) int {
-// 	sum := 0
+func partOne(grid [][]int) int {
+	sum := 0
 
-// 	for i := 1; i < len(grid)-1; i++ {
-// 		for j := 1; j < len(grid[0])-1; j++ {
-// 			if checkAbove(grid, i, j) || checkBelow(grid, i, j) || checkRight(grid, i, j) || checkLeft(grid, i, j) {
-// 				sum++
-// 			}
+	for i := 1; i < len(grid)-1; i++ {
+		for j := 1; j < len(grid[0])-1; j++ {
+			if checkAboveP1(grid, i, j) || checkBelowP1(grid, i, j) || checkRightP1(grid, i, j) || checkLeftP1(grid, i, j) {
+				sum++
+			}
 
-// 		}
-// 	}
-// 	sum += len(grid)*2 + len(grid[0])*2 - 4
-// 	return sum
-// }
+		}
+	}
+	// add the trees at the boundary
+	sum += len(grid)*2 + len(grid[0])*2 - 4
+	return sum
+}
 
 func partTwo(grid [][]int) int {
 	heights := []int{}
 
 	for i := 1; i < len(grid)-1; i++ {
 		for j := 1; j < len(grid[0])-1; j++ {
-			// if i == 3 && j == 2 {
-			// fmt.Println(fmt.Sprintf("checking for i=%v, j=%v, val=%v", i, j, grid[i][j]))
-			heights = append(heights, checkAbove(grid, i, j)*checkBelow(grid, i, j)*checkLeft(grid, i, j)*checkRight(grid, i, j))
-			// }
+			heights = append(heights, checkAboveP2(grid, i, j)*checkBelowP2(grid, i, j)*checkLeftP2(grid, i, j)*checkRightP2(grid, i, j))
 		}
 	}
 	sort.Slice(heights, func(i, j int) bool {
@@ -71,29 +65,60 @@ func partTwo(grid [][]int) int {
 	return heights[0]
 }
 
-func checkAbove(grid [][]int, i, j int) int {
+func checkAboveP1(grid [][]int, i, j int) bool {
+	for i2 := 0; i2 <= i-1; i2++ {
+		if grid[i][j] <= grid[i-1-i2][j] {
+			return false
+		}
+	}
+	return true
+}
+
+func checkBelowP1(grid [][]int, i, j int) bool {
+	limit := len(grid) - 1 - i
+	for i2 := 0; i2 < limit; i2++ {
+		if grid[i][j] <= grid[i+1+i2][j] {
+			return false
+		}
+	}
+	return true
+}
+
+func checkLeftP1(grid [][]int, i, j int) bool {
+	for j2 := 0; j2 <= j-1; j2++ {
+		if grid[i][j] <= grid[i][j-1-j2] {
+			return false
+		}
+	}
+	return true
+}
+
+func checkRightP1(grid [][]int, i, j int) bool {
+	limit := len(grid[0]) - 1 - j
+	for j2 := 0; j2 < limit; j2++ {
+		if grid[i][j] <= grid[i][j+1+j2] {
+			return false
+		}
+	}
+	return true
+}
+
+func checkAboveP2(grid [][]int, i, j int) int {
 	i2 := 0
 	for i2 <= i-1 {
-		// fmt.Println(fmt.Sprintf("looking at for i=%v, j=%v, val=%v", i-1-i2, j, grid[i-1-i2][j]))
-
 		if grid[i][j] <= grid[i-1-i2][j] {
-			fmt.Println("blocking tree is", grid[i-1-i2][j])
-
 			break
 		}
 		i2++
 	}
-	if i-1-i2 == -1 {
-		fmt.Println("at the end so above is", i2)
+
+	if i == i2 {
 		return i2
 	}
-	fmt.Println("above is", i2+1)
-	fmt.Println("we go up to is", i-1-i2)
-
 	return i2 + 1
 }
 
-func checkBelow(grid [][]int, i, j int) int {
+func checkBelowP2(grid [][]int, i, j int) int {
 	limit := len(grid) - 1 - i
 	i2 := 0
 	for i2 < limit {
@@ -103,16 +128,13 @@ func checkBelow(grid [][]int, i, j int) int {
 		i2++
 	}
 
-	if i+1+i2 == len(grid) {
-		fmt.Println("below is", i2)
+	if i2 == limit {
 		return i2
 	}
-	fmt.Println("below is", i2+1)
-
 	return i2 + 1
 }
 
-func checkLeft(grid [][]int, i, j int) int {
+func checkLeftP2(grid [][]int, i, j int) int {
 	j2 := 0
 	for j2 <= j-1 {
 		if grid[i][j] <= grid[i][j-1-j2] {
@@ -121,16 +143,13 @@ func checkLeft(grid [][]int, i, j int) int {
 		j2++
 	}
 
-	if j-1-j2 == -1 {
-		fmt.Println("left is", j2)
+	if j2 == j {
 		return j2
 	}
-	fmt.Println("left is", j2+1)
-
 	return j2 + 1
 }
 
-func checkRight(grid [][]int, i, j int) int {
+func checkRightP2(grid [][]int, i, j int) int {
 	j2 := 0
 	limit := len(grid[0]) - 1 - j
 	for j2 < limit {
@@ -139,14 +158,9 @@ func checkRight(grid [][]int, i, j int) int {
 		}
 		j2++
 	}
-	fmt.Println("we go up to is", j+1+j2)
-	fmt.Println(len(grid))
 
-	if j+1+j2 == len(grid) {
-		fmt.Println("at end so right is", j2)
+	if j2 == limit {
 		return j2
 	}
-	fmt.Println("right is", j2+1)
-
 	return j2 + 1
 }
